a45eec28bb263a91614f8c92ec06fec9aaeda1e8
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-18 16:36:19 +0100

    Added a welcome message and bad url message

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.use(logger('dev'));
 server.get('/', (req, res) =>
   res.status(200).json({
     status: 200,
-    message: 'Of this API I have I give unto thee!',
+    message: 'Welcome... this API I have I give unto thee!',
   }),
 );
 
